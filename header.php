6b70d35b1fa9f4f95fe1a32360a624745febe8ff
Andrew S. Gardner <asg@andrewsgardner.com>
2016-10-29 21:12:58 -0400

    Remove commented out favicon link

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
         <meta charset="<?php bloginfo('charset'); ?>">
         <title><?php bloginfo('name'); ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- <link rel="icon" type="image/png" href="#"> -->
         <?php wp_head(); ?>
     </head>
 
